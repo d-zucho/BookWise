3ebccca3a245e02e18d566b51a3410162ba2b4d1
d-zucho <danny.nico.dev@outlook.com>
2025-03-19 02:10:18 -0700

    add starter nav and logo

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from 'react'
+import Header from "@/components/Header";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <main className='root-container'>
       <div className='mx-auto max-w-7xl'>
-        Header here
+        <Header />
         <div className='mt-20 pb-20'>{children}</div>
       </div>
     </main>
