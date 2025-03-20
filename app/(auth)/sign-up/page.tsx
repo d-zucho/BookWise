3e26c5bed33c7e45462b4725d037c6b796e08ca3
d-zucho <danny.nico.dev@outlook.com>
2025-03-19 22:51:45 -0700

    add initial auth form with zod validation

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,25 @@
+'use client'
+import AuthForm from '@/components/AuthForm'
+import { signUpSchema } from '@/lib/validations'
 import React from 'react'
 
 const SignUp = () => {
-  return <div>SignUp</div>
+  return (
+    <div>
+      <AuthForm
+        type='SIGN_UP'
+        schema={signUpSchema}
+        defaultValues={{
+          email: '',
+          password: '',
+          fullName: '',
+          universityId: 0,
+          universityCard: '',
+        }}
+        onSubmit={() => {}}
+      />
+    </div>
+  )
 }
 
 export default SignUp
